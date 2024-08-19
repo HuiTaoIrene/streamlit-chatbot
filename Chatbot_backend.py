a630a8ede4a8c8594760ecb03bdc788e428a02d8
Hayleee Tao <xiaobaishu67@gmail.com>
2024-08-19 16:39:37 -0700

    Update V1

--- Chatbot_backend.py
+++ Chatbot_backend.py
@@ -4,7 +4,7 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationSummaryBufferMemory
 from langchain_aws import ChatBedrock
 #2a Write a function for invoking model- client connection with Bedrock with profile, model_id & Inference params- model_kwargs
-def demo_chatbot(input_text):
+def demo_chatbot():
     demo_llm=ChatBedrock(
        credentials_profile_name='default',
        model_id='anthropic.claude-v2:1',
@@ -13,27 +13,27 @@ def demo_chatbot(input_text):
            "temperature": 0.1,
            "top_p": 0.9,
            "stop_sequences": ["\n\nHuman:"]} )
-    #return demo_llm
+    return demo_llm
 #2b Test out the LLM with Predict method instead use invoke method
-    return demo_llm.invoke(input_text)
-response=demo_chatbot(input_text="Hi, what is your name?")
-print(response)
+    #return demo_llm.invoke(input_text)
+#response=demo_chatbot(input_text="Hi, what is the temperature in new york in January?")
+#print(response)
 
-# #3 Create a Function for  ConversationSummaryBufferMemory  (llm and max token limit)
-# def demo_memory():
-#     llm_data=demo_chatbot()
-#     memory=ConversationSummaryBufferMemory(llm=llm_data,max_token_limit=300)
-#     return memory
+#3 Create a Function for  ConversationSummaryBufferMemory  (llm and max token limit)
+def demo_memory():
+    llm_data=demo_chatbot()
+    memory=ConversationSummaryBufferMemory(llm=llm_data,max_token_limit=300)
+    return memory
 
-# #4 Create a Function for Conversation Chain - Input text + Memory
-# def demo_conversation(input_text,memory):
-#     llm_chain_data=demo_chatbot()
-#     llm_conversation=ConversationChain(llm=llm_chain_data,memory=memory,verbose=True)
+#4 Create a Function for Conversation Chain - Input text + Memory
+def demo_conversation(input_text,memory):
+    llm_chain_data=demo_chatbot()
+    llm_conversation=ConversationChain(llm=llm_chain_data,memory=memory,verbose=True)
 
-# #5 Chat response using invoke (Prompt template)
-#     chat_reply=llm_conversation.invoke(input_text)
-#     return chat_reply['response']
+#5 Chat response using invoke (Prompt template)
+    chat_reply=llm_conversation.invoke(input_text)
+    return chat_reply['response']
 
-# #1 https://python.langchain.com/v0.1/docs/integrations/llms/bedrock/
-# #pip install -U langchain-aws
-# #pip install anthropic
+#1 https://python.langchain.com/v0.1/docs/integrations/llms/bedrock/
+#pip install -U langchain-aws
+#pip install anthropic
---
+++ test.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+# Display title and message
+st.title("Chatbot Application")
+st.write("This is a simple chatbot application.")
